4baf2d202a563338d139a7e0f9b7c30781e40d63
murarie <murarimishra1611@gmail.com>
2025-11-07 20:52:07 +0530

    minor fixes for to deploy build

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -13,7 +13,7 @@ const Agent = ({ userName }: AgentProps) => {
     const isSpeaking = true;
     const messages = [
         'Whats your name?',
-        'My name is John Doe, nice to meet you!'
+        'My name is murari, nice to meet you!'
     ];
     const lastMessage = messages[messages.length - 1];
 
